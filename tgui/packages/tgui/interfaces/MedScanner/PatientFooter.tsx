d2eab7e09f2881a4f42e1f7479afee9c9b4a5349
Pak2024 <dushneff2017@yandex.ru>
2026-08-08 14:16:03 +0500

    Кровь

--- tgui/packages/tgui/interfaces/MedScanner/PatientFooter.tsx
+++ tgui/packages/tgui/interfaces/MedScanner/PatientFooter.tsx
@@ -3,24 +3,24 @@ import {
   Icon,
   LabeledList,
   NoticeBox,
+  ProgressBar,
   Section,
+  Stack,
+  Tooltip,
 } from 'tgui-core/components';
 
 import { useBackend } from '../../backend';
 import {
-  COUNTER_MAX_SIZE,
   SPACING_PIXELS,
   TEMP_LEVELS_TO_DATA,
 } from './constants';
-import { BloodColors, MedScannerData, TempLevels } from './data';
+import { MedScannerData, TempLevels } from './data';
 import { getBloodColor } from './helpers';
 import { MedBoxedTag } from './MedBoxedTag';
-import { MedCounter } from './MedCounter';
 
 export function PatientFooter() {
   const { data } = useBackend<MedScannerData>();
   const {
-    dead,
     blood_amount,
     regular_blood_amount,
     blood_type,
@@ -44,48 +44,40 @@ export function PatientFooter() {
           label="Blood Volume"
           tooltip="Bloodloss causes symptoms that start as suffocation and pain, but get significantly worse as more blood is lost. Blood can be restored by eating and taking Isotonic solution."
         >
-          <Icon
-            name={total_flow_rate ? 'arrow-down' : 'arrow-up'}
-            mr={SPACING_PIXELS}
-            color={
-              bloodColors.background === BloodColors.FineBG
-                ? 'white'
-                : bloodColors.background
-            }
-          />
-          <Box
-            mr={SPACING_PIXELS}
-            inline
-            color={
-              bloodColors.background === BloodColors.FineBG
-                ? 'white'
-                : bloodColors.background
-            }
-            bold={bloodColors.background !== BloodColors.FineBG ? true : false}
-          >
-            {Math.trunc((blood_amount / regular_blood_amount) * 100)}%
-          </Box>
-          <MedCounter
-            current={Math.trunc(blood_amount)}
-            max={Math.trunc(regular_blood_amount)}
-            units={`cl${total_flow_rate ? ` (${total_flow_rate}cl/2s)` : ''}`}
-            currentColor={bloodColors.darker}
-            maxColor={bloodColors.darker}
-            currentSize={COUNTER_MAX_SIZE}
-            mr={SPACING_PIXELS}
-          />
-          <MedBoxedTag
-            mr={SPACING_PIXELS}
-            textColor={bloodColors.foreground}
-            backgroundColor={bloodColors.background}
-          >
-            {blood_type}
-          </MedBoxedTag>
-          {!!internal_bleeding && (
-            <MedBoxedTag textColor="white" backgroundColor="red">
-              INT. BLEEDING
-            </MedBoxedTag>
-          )}
+          <Stack align="center">
+            <Stack.Item>
+              <MedBoxedTag
+                textColor={bloodColors.foreground}
+                backgroundColor={bloodColors.background}
+              >
+                {blood_type}
+              </MedBoxedTag>
+            </Stack.Item>
+            <Stack.Item grow={1} ml={SPACING_PIXELS}>
+              <Tooltip
+                content={
+                  <Box>
+                    {Math.trunc(blood_amount)}/{Math.trunc(regular_blood_amount)}cl
+                    {total_flow_rate ? ` (${total_flow_rate}cl/2s)` : ''}
+                  </Box>
+                }
+              >
+                <ProgressBar
+                  value={blood_amount / regular_blood_amount}
+                  ranges={{
+                    good: [0.8, Infinity],
+                    red: [-Infinity, 0.8],
+                  }}
+                >
+                  <Icon
+                    name={total_flow_rate ? 'arrow-down' : 'arrow-up'}
+                    mr="4px"
+                  />
+                  {Math.trunc((blood_amount / regular_blood_amount) * 100)}%
+                </ProgressBar>
+              </Tooltip>
+            </Stack.Item>
+          </Stack>
         </LabeledList.Item>
         <LabeledList.Item label="Body Temperature" color={tempData.background}>
           <Box inline bold={body_temperature.level !== TempLevels.OK}>
@@ -103,6 +95,11 @@ export function PatientFooter() {
         </LabeledList.Item>
         <LabeledList.Item label="Pulse">{pulse}</LabeledList.Item>
       </LabeledList>
+      {!!internal_bleeding && (
+        <NoticeBox color={'red'} mt={'8px'} mb={'0px'} warning>
+          Internal Bleeding Detected!
+        </NoticeBox>
+      )}
       {!!infection && (
         <NoticeBox color="orange" mt="8px" mb="0px">
           {infection}
